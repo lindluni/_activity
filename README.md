7e8c71442647008c25fb309644a5cafea2d1bfaa
Jason Macgowan <1389531+jasonmacgowan@users.noreply.github.com>
2021-03-25 16:42:50 -0400

    create scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # \_activity
+
+Collection of scripts to find and log user activity.
+
+See [template.js](./scripts/template.js) for a starting point using an authenticated GitHub client.
---
+++ scripts/template.js
@@ -0,0 +1,49 @@
+const yargs = require("yargs");
+const { getGitHubApp } = require("../lib/github");
+const { getDateFromDaysAgo } = require("../lib/utils");
+
+async function main(auth, owner, since) {
+  const app = getGitHubApp(auth, owner, since);
+  app.log.info("Prepped and ready!");
+}
+
+// node scripts/template.js --owner department-of-veterans-affairs --days 91
+if (require.main === module) {
+  const requiredEnvs = ["INACTIVE_APP_ID", "INACTIVE_CLIENT_ID", "INACTIVE_CLIENT_SECRET", "INACTIVE_PRIVATE_KEY"];
+
+  for (const requiredEnv of requiredEnvs) {
+    if (!Object.prototype.hasOwnProperty.call(process.env, requiredEnv)) {
+      console.error(`${requiredEnv} is missing from environment.`);
+      process.exit(1);
+    }
+  }
+
+  const { argv } = yargs
+    .options("owner", {
+      alias: "o",
+      description: "Owner or Organization with the repos",
+      global: true,
+      demandOption: true,
+    })
+    .option("days", {
+      alias: "d",
+      description: "Days in the past to start from",
+      global: true,
+      demandOption: true,
+    });
+
+  // configure GitHub App auth details as object
+  const app = {
+    appId: process.env.INACTIVE_APP_ID,
+    clientId: process.env.INACTIVE_CLIENT_ID,
+    clientSecret: process.env.INACTIVE_CLIENT_SECRET,
+    privateKey: process.env.INACTIVE_PRIVATE_KEY,
+  };
+
+  const { owner, days } = argv;
+  const since = getDateFromDaysAgo(days);
+
+  main(app, owner, since);
+}
+
+module.exports = main;
