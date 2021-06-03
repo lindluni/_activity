9dd6098cc3193db891adb893d4deb31dd67dd75f
Brett Logan <lindluni@github.com>
2021-06-03 10:15:04 -0400

    Update deps due to LowDB issue
    Version 2.x of LowDB will not function until this change is merged:
    https://github.com/typicode/lowdb/pull/472

--- automation/lib/database.js
+++ automation/lib/database.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-import { low } from 'lowdb'
+const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
 const adapter = new FileSync('db.json')
